48ff38cc9745f44ca28794e85cdec81410c9430c
PranavEranki <pranav.eranki@gmail.com>
2020-09-03 22:51:42 -0700

    adad

--- js/modal2.js
+++ js/modal2.js
@@ -1,11 +1,25 @@
 var modal1 = document.getElementById("myModal1");
 var btn1 = document.getElementById("trigger-modal-1");
 var span1 = document.getElementsByClassName("close1")[0];
+// var containerTop = document.getElementsByClassName("site-blocks-cover-1")[0];
+// var images = document.getElementsByClassName('modal-class'); 
+// var containerBotom = document.getElementsByClassName("site-section")[0];
+// var containerBotom = document.getElementById("stories-div");
 btn1.addEventListener("click", function(){
   modal1.style.display = "block";
+  // containerTop.className = "site-blocks-cover-1 is-blurred";
+  // containerBottom.className = "site-section is-blurred";
+  // for(var i = 0; i < images.length; i++) {
+  //     images[i].filter="blur(5px)";
+  // }
 }, false);
 span1.addEventListener("click", function() {
   modal1.style.display = "none";
+  // containerTop.className = "site-blocks-cover-1 no-blurred";
+  // containerBottom.className = "site-section no-blurred";
+//   for(var i = 0; i < images.length; i++) {
+//     images[i].filter="blur(0px)";
+// }
 })
 
 var modal2 = document.getElementById("myModal2");
@@ -13,9 +27,13 @@ var btn2 = document.getElementById("trigger-modal-2");
 var span2 = document.getElementsByClassName("close2")[0];
 btn2.addEventListener("click", function(){
   modal2.style.display = "block";
+  // containerTop.className = "site-blocks-cover-1 is-blurred";
+  // containerBottom.className = "site-section is-blurred";
 }, false);
 span2.addEventListener("click", function() {
   modal2.style.display = "none";
+  // containerTop.className = "site-blocks-cover-1 no-blurred";
+  // containerBottom.className = "site-section no-blurred";
 })
 
 var modal3 = document.getElementById("myModal3");
@@ -23,9 +41,13 @@ var btn3 = document.getElementById("trigger-modal-3");
 var span3 = document.getElementsByClassName("close3")[0];
 btn3.addEventListener("click", function(){
   modal3.style.display = "block";
+  // containerTop.className = "site-blocks-cover-1 is-blurred";
+  // containerBottom.className = "site-section is-blurred";
 }, false);
 span3.addEventListener("click", function() {
   modal3.style.display = "none";
+  // containerTop.className = "site-blocks-cover-1 no-blurred";
+  // containerBottom.className = "site-section no-blurred";
 })
 
 var modal4 = document.getElementById("myModal4");
@@ -33,9 +55,13 @@ var btn4 = document.getElementById("trigger-modal-4");
 var span4 = document.getElementsByClassName("close4")[0];
 btn4.addEventListener("click", function(){
   modal4.style.display = "block";
+  // containerTop.className = "site-blocks-cover-1 is-blurred";
+  // containerBottom.className = "site-section is-blurred";
 }, false);
 span4.addEventListener("click", function() {
   modal4.style.display = "none";
+  // containerTop.className = "site-blocks-cover-1 no-blurred";
+  // containerBottom.className = "site-section no-blurred";
 })
 
 var modal5 = document.getElementById("myModal5");
@@ -43,9 +69,13 @@ var btn5 = document.getElementById("trigger-modal-5");
 var span5 = document.getElementsByClassName("close5")[0];
 btn5.addEventListener("click", function(){
   modal5.style.display = "block";
+  // containerTop.className = "site-blocks-cover-1 is-blurred";
+  // containerBottom.className = "site-section is-blurred";
 }, false);
 span5.addEventListener("click", function() {
   modal5.style.display = "none";
+  // containerTop.className = "site-blocks-cover-1 no-blurred";
+  // containerBottom.className = "site-section no-blurred";
 })
 
 var modal6 = document.getElementById("myModal6");
@@ -53,9 +83,13 @@ var btn6 = document.getElementById("trigger-modal-6");
 var span6 = document.getElementsByClassName("close6")[0];
 btn6.addEventListener("click", function(){
   modal6.style.display = "block";
+  // containerTop.className = "site-blocks-cover-1 is-blurred";
+  // containerBottom.className = "site-section is-blurred";
 }, false);
 span6.addEventListener("click", function() {
   modal6.style.display = "none";
+  // containerTop.className = "site-blocks-cover-1 no-blurred";
+  // containerBottom.className = "site-section no-blurred";
 })
 
 var modal7 = document.getElementById("myModal7");
@@ -63,9 +97,13 @@ var btn7 = document.getElementById("trigger-modal-7");
 var span7 = document.getElementsByClassName("close7")[0];
 btn7.addEventListener("click", function(){
   modal7.style.display = "block";
+  // containerTop.className = "site-blocks-cover-1 is-blurred";
+  // containerBottom.className = "site-section is-blurred";
 }, false);
 span7.addEventListener("click", function() {
   modal7.style.display = "none";
+  // containerTop.className = "site-blocks-cover-1 no-blurred";
+  // containerBottom.className = "site-section no-blurred";
 })
 
 var modal8 = document.getElementById("myModal8");
@@ -73,7 +111,11 @@ var btn8 = document.getElementById("trigger-modal-8");
 var span8 = document.getElementsByClassName("close8")[0];
 btn8.addEventListener("click", function(){
   modal8.style.display = "block";
+  // containerTop.className = "site-blocks-cover-1 is-blurred";
+  // containerBottom.className = "site-section is-blurred";
 }, false);
 span8.addEventListener("click", function() {
   modal8.style.display = "none";
+  // containerTop.className = "site-blocks-cover-1 no-blurred";
+  // containerBottom.className = "site-section no-blurred";
 })
