f7233b3bb2f0846bda66c2d690f2b5dafaefc3bf
PranavEranki <pranav.eranki@gmail.com>
2020-09-03 21:59:49 -0700

    Adding modal blurring

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@ var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("trigger-modal");
+var container = document.getElementsByClassName("site-blocks-cover")[0];
 console.log(btn);
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -10,16 +11,19 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+  container.className = "site-blocks-cover is-blurred";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  container.className = "site-blocks-cover no-blurred";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    container.className = "site-blocks-cover no-blurred";
   }
 }
