1f5ebb6af31c42f70c9a20e72f9c63a2d8a16053
PranavEranki <pranav.eranki@gmail.com>
2020-09-13 10:47:49 -0700

    Aesthetic changes

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ jQuery(document).ready(function($) {
 	"use strict";
 
 	
-	$(".loader").delay(2000).fadeOut("slow");
-  $("#overlayer").delay(2000).fadeOut("slow");	
+	$(".loader").delay(3500).fadeOut("slow");
+  $("#overlayer").delay(3500).fadeOut("slow");	
 
 	var siteMenuClone = function() {
 
